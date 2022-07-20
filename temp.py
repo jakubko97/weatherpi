a0f04d11901a4ff55d71692d0f3359a0063129d1
Jakub Jančík <xjancikj@stuba.sk>
2022-07-20 21:40:26 +0200

    delete unused functions, fixes

--- temp.py
+++ temp.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import Adafruit_DHT
 import datetime
-from ISStreamer.Streamer import Streamer
 import time as t
-import board
 from influxdb import InfluxDBClient
 
 # --------- User Settings ---------
-SENSOR_LOCATION_NAME = "Jakubs Room"
+SENSOR_LOCATION_NAME = "In backyard"
 BUCKET_NAME = ":partly_sunny: Room Temperatures"
 BUCKET_KEY = "dht22sensor"
 ACCESS_KEY = "1892"
@@ -22,8 +20,6 @@ ifdb   = "home"
 ifhost = "127.0.0.1"
 ifport = 8086
 measurement_name = "temperature"
-
-streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 
 sensor = 22
 gpio = 4
@@ -40,11 +36,9 @@ while True:
     humidity = format(humidity,".2f")
     temperature = format(temperature,".2f")
 
-    streamer.log(SENSOR_LOCATION_NAME + " Temperature(C)", temperature)
-    print(" Temperature(C)", temperature)
+    print("Temperature(C)", temperature)
 
-    streamer.log(SENSOR_LOCATION_NAME + " Humidity(%)", humidity)
-    print(" Humidity(%)", humidity)
+    print("Humidity(%)", humidity)
 
 
     # take a timestamp for this measurement
@@ -56,8 +50,8 @@ while True:
         "measurement": measurement_name,
         "time": time,
         "fields": {
-            "temperature": temperature,
-            "humidity": humidity,
+            "temperature": float(temperature),
+            "humidity": float(humidity),
             "location": SENSOR_LOCATION_NAME
         }
     }
@@ -67,5 +61,4 @@ while True:
 
     # write the measurement
     ifclient.write_points(body)
-    streamer.flush()
     t.sleep(60*MINUTES_BETWEEN_READS)
