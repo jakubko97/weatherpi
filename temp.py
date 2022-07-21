4b00f648f18a06e73e2fc057b743f1c818f11810
Jakub Jančík <xjancikj@stuba.sk>
2022-07-21 13:20:20 +0200

    add alert as email sender

--- temp.py
+++ temp.py
@@ -4,6 +4,14 @@ import datetime
 import time as t
 from influxdb import InfluxDBClient
 
+# Sendmail -text file - only .py
+ 
+import smtplib, sys
+ 
+from email. mime. text import MIMEText
+ 
+from email. header import Header
+
 # --------- User Settings ---------
 SENSOR_LOCATION_NAME = "In backyard"
 BUCKET_NAME = ":partly_sunny: Room Temperatures"
@@ -23,6 +31,33 @@ measurement_name = "temperature"
 
 sensor = 22
 gpio = 4
+
+def send_email(value):
+    frm = 'j5jancik@gmail.com'
+ 
+    to = 'j4jancik@gmail.com'
+ 
+    subj = 'Temperature alert'
+ 
+    msg = 'Sensor measured temperature ' + value + ' celsius.'
+ 
+    try:
+         mime = MIMEText (msg, 'plain', 'utf -8')
+         mime ['From'] = frm
+         mime ['To'] = to
+         mime ['Subject'] = (subj)
+         smtp = smtplib. SMTP ("smtp.gmail.com")
+         smtp. starttls ()
+         smtp. login ("j5jancik@gmail.com", "npusafyghfwdjbwi")
+         smtp. sendmail (frm, [to], mime. as_string ())
+         smtp. quit ()
+ 
+    except:
+        print ("An error occurred while sending the e-mail:",
+        sys. exc_info ())
+
+
+alert = False
 
 while True:
     try:
@@ -61,4 +96,8 @@ while True:
 
     # write the measurement
     ifclient.write_points(body)
+    if(alert == False and float(temperature) >= 45.5):
+        send_email(temperature)
+        alert = True
+
     t.sleep(60*MINUTES_BETWEEN_READS)
